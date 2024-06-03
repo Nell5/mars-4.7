997acab112ccc6e5d24d4731aea0b17c259efce2
Nell5 <neeennever@gmail.com>
2024-06-03 15:14:03 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let arr = ["Olma", "Kiwi", "Anor"]
+
+// arr.push("Nok")
+
+// arr.pop()
+
+// arr.unshift("Banan")
+
+// arr.shift()
+
+// arr.length
+
+// alert(arr)
+
+alert(arr)
+
+let a = confirm("Mahsulot qoshasmi")
+
+if (a == true) {
+   let b = prompt("mahsulot nomi")
+   arr.push(b)
+   alert(arr)
+} else if (a == false)
+   alert("mahsulot qoshmadiz");
+
+let b = confirm("Mahsulot optashismi")
+
+if (b == true) {
+   arr.pop()
+}
+
